7ba59ab3f828c747e55c6f8ec79881f0f29c3309
Nolan Dyke <nolan.g.dyke@gmail.com>
2020-03-19 20:41:20 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,8 @@ require 'yaml'
 def load_library(path) 
   emoticons = YAML.load_file(path)
   emoticon_hash = Hash.new 
-  emoticon_hash["get_emoticon"]
+  emoticon_hash["get_emoticon"] = Hash.new 
+  emoticon_hash[""]
 end
 
 def get_japanese_emoticon
