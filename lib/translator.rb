468d431e13c0aada8f6ec0e3834874c4605db0ba
Nolan Dyke <nolan.g.dyke@gmail.com>
2020-03-19 20:43:38 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,7 +8,7 @@ def load_library(path)
   
   emoticons.each do |english_word, emoticon_set|
     emoticon_hash["get_emoticon"][emoticon_set.first] = emoticon_set.last 
-    
+    emoticon_hash["get_meaning"][]
 end
 
 def get_japanese_emoticon
